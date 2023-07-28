7c4dc9e4b0454c73263a3290e7db2fe65cbfbd80
Jin Kwon Kim <cosy363@naver.com>
2023-07-28 15:36:23 +0900

    added cognito user login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Amplify from "aws-amplify";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
+import awsconfig from "./awsconfig";
 
-function App() {
+Amplify.configure(awsconfig);
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AmplifyAuthenticator>
+      <div>
+        ONLY LOGGED IN USERS CAN SEE THIS
+      </div>
+    </AmplifyAuthenticator>
   );
-}
-
-export default App;
+};
