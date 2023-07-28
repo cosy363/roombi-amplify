04999220d474e0802acb73601e4b8496e19896d4
Jin Kwon Kim <cosy363@naver.com>
2023-07-28 16:42:49 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import { Amplify } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
+import { I18n } from 'aws-amplify';
+import { translations } from '@aws-amplify/ui-react';
+
 import awsconfig from './awsconfig';
+
+I18n.putVocabularies(translations);
+I18n.setLanguage('ko');
+
+I18n.putVocabularies({
+  ko: {
+    'Enter your Username': '아이디 입력',
+    'Enter your Password': "비밀번호 입력",
+    'Please confirm your Password': "비밀번호 재입력",
+  }
+});
 
 Amplify.configure(awsconfig);
 
 export default function App() {
   return (
-    <Authenticator>
+    <Authenticator signUpAttributes={[
+      'email',
+    ]} variation="modal">
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user.username}</h1>
